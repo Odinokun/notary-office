c8ae15f10c7d89e5fe2f23c01eb43315e143b4d1
odinokun <odinokun@gmail.com>
2018-12-23 15:18:14 +0200

    add new-year elements

--- src/js/modules/bubbleFooter.js
+++ src/js/modules/bubbleFooter.js
@@ -3,7 +3,7 @@ module.exports = function() {
   // begin Всплывающий подвал
   $(window).on('load', function() {
     $('.footer').addClass('active');
-  })
+  });
   // end   Всплывающий подвал
 
 };
