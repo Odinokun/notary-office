1ff67e5256f68869875805575abae20fb7269e9f
odinokun <odinokun@gmail.com>
2023-10-13 21:31:34 +0200

    feat: update npm ang gulp

--- gulp/tasks/sass.js
+++ gulp/tasks/sass.js
@@ -1,12 +1,13 @@
 'use strict';
 
 module.exports = function() {
+  const sass = require('gulp-sass')(require('sass'));
 
   $.gulp.task('sass', function() {
     return $.gulp.src('./src/style/app.scss')
       .pipe($.gp.sourcemaps.init())
-      .pipe($.gp.sass()).on('error', $.gp.notify.onError({ title: 'Style SASS'}))
-      .pipe($.gp.autoprefixer({ browsers: $.config.autoprefixerConfig }))
+      .pipe(sass()).on('error', $.gp.notify.onError({ title: 'Style SASS'}))
+      .pipe($.gp.autoprefixer({grid: true}))
       .pipe($.gp.csso())
       .pipe($.gp.sourcemaps.write())
       .pipe($.gulp.dest($.config.root + '/assets/css'))
